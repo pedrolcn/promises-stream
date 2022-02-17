80000e10258fd12c6a6eb2229f1f89b3d1ee553f
Pedro Nascimento <pedrolcn@gmail.com>
2022-02-17 14:01:30 -0300

    feat: Add toStream function

---
+++ index.ts
@@ -0,0 +1,65 @@
+import EventEmitter, { once } from "events";
+import { setTimeout } from 'timers/promises'
+
+export type FalliblePromiseResult<T> =
+    { error: false, value: T } |
+    { error: true, value: any};
+
+/**
+ * A queue on which the dequeue operation returns a promise which does not resolve until there is an element to dequeue.
+ */
+class YieldQueue<T> {
+    private readonly queue: T[] = [];
+
+    private emitter: EventEmitter = new EventEmitter();
+
+    enqueue(el: T): number {
+        const size = this.queue.unshift(el);
+        this.emitter.emit("enqueue");
+        return size;
+    }
+
+    async dequeue(): Promise<T> {
+        const el = this.queue.pop();
+        if (el) {
+            return el;
+        }
+
+        await once(this.emitter, 'enqueue');
+        return this.queue.pop()!;
+    }
+
+
+}
+
+async function * toStream<T>(promises: Promise<T>[]): AsyncIterable<FalliblePromiseResult<T>> {
+    const yieldQueue = new YieldQueue<FalliblePromiseResult<T>>();
+    let pendingPromisesCount = promises.length;
+
+    for (const promise of promises) {
+        promise.then(
+            (val) => yieldQueue.enqueue({ error: false, value: val }),
+            (err) => yieldQueue.enqueue({ error: true, value: err}),
+        )
+    }
+    
+    while (pendingPromisesCount > 0) {
+        const result = await yieldQueue.dequeue();
+        pendingPromisesCount -= 1;
+        yield result;
+    }
+}
+
+
+async function main() {
+    const promises = Array.from({ length: 10 }, () => {
+        const delay = Math.floor(Math.random() * 1000);
+        return setTimeout(delay, delay);
+    });
+
+    for await (const val of toStream(promises)) {
+        console.log(val);
+    }
+}
+
+main();
